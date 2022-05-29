e2ac07ec9b4325ac4b3b11c7a019b3bc5b1237e4
shs395 <shs395@ajou.ac.kr>
2022-05-30 00:34:46 +0900

    chore: Set develop network to ganache network

--- truffle-config.js
+++ truffle-config.js
@@ -4,9 +4,17 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  networks: {
-    develop: {
-      port: 8545
+  compilers: {
+    solc: {
+      version: "^0.8.0"
     }
+  },
+  networks: {
+    // ganache 
+    develop: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777" // Match any network id
+    },
   }
 };
